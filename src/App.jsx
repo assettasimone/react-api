3c24034790d5e6b3c27df590ba3f41a109ab994d
assettasimone <assettasimone@gmail.com>
2025-10-16 16:10:46 +0200

    add actor card layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,33 @@ function App() {
 
   useEffect(fetchActors, [])
 
-  console.log(actors);
-
-
   return (
     <>
+      <section className="container py-4">
 
+        <div className="row align-items-stretch">
+          {actors.map(actor => (
+            <div key={actor.id} className="col-3 mb-4">
+              <div className="card"  >
+                <div className='object-fit-cover'
+                  style={{
+                    backgroundImage: `url( ${actor.image})`,
+                    height: '250px',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                  }} ></div>
+
+                <div className="card-body">
+                  <h3 className="card-title">{actor.name}</h3>
+                  <p className='card-text'>{actor.}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </section >
     </>
   )
 }
